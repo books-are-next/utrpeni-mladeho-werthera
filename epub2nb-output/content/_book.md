9d32bc4b9f7e7d05793e05589639e3a81bb37848
jan-martinek <honza.martinek@gmail.com>
2021-12-01 21:21:25 +0000

    Deploying to docs from @ books-are-next/utrpeni-mladeho-werthera@cc877416a63322a914b1185ba76c8a242392fd18 🚀

---
+++ epub2nb-output/content/_book.md
@@ -0,0 +1,39 @@
+---
+outputs: meta
+slug: book
+languageCode: cs
+meta:
+  title: Utrpení mladého Werthera
+  identifier: 99527d21-3431-4edd-83e1-e87f65b92a9f
+  author: Johann Wolfgang Goethe
+  publisher: Městská knihovna v Praze
+  modified: '2021-10-29T08:41:30.619Z'
+  languageCode: cs
+readingOrder:
+  - utrpeni_mladeho_werthera_001.html
+  - utrpeni_mladeho_werthera_002.html
+  - utrpeni_mladeho_werthera_006.html
+  - utrpeni_mladeho_werthera_007.html
+  - utrpeni_mladeho_werthera_008.html
+  - utrpeni_mladeho_werthera_009.html
+  - utrpeni_mladeho_werthera_010.html
+  - colophon.html
+tocBase:
+  - id: section-1
+    children:
+      - link: utrpeni_mladeho_werthera_006.html
+        title: PRVNÍ DÍL
+      - link: utrpeni_mladeho_werthera_008.html
+        title: DRUHÝ DÍL
+      - link: utrpeni_mladeho_werthera_010.html
+        title: Poznámky pod čarou
+    isSection: true
+static:
+  - style
+  - scripts
+  - title
+  - fonts
+  - resources
+  - template-images
+  - favicon.png
+---
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-cc87741';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./utrpeni_mladeho_werthera_001.html","./utrpeni_mladeho_werthera_002.html","./utrpeni_mladeho_werthera_006.html","./utrpeni_mladeho_werthera_007.html","./utrpeni_mladeho_werthera_008.html","./utrpeni_mladeho_werthera_009.html","./utrpeni_mladeho_werthera_010.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.png","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
