b6938b38e761cc138fd0f2d4c9ad610bc1ac91b4
jan-martinek <jan-martinek@users.noreply.github.com>
2022-06-13 22:03:56 +0000

    Deploying to docs from @ books-are-next/utrpeni-mladeho-werthera@6ee45fd4261617f1c60b4eeba2ae1ecc65120763 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-c5845ad';
+const CACHE = 'cache-6ee45fd';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
@@ -1019,7 +1019,7 @@ async function cache(context) {
         .open(CACHE)
         .then(cache => {
         console.log('[' + context + '] Opened cache');
-        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./promo.html","./utrpeni_mladeho_werthera_005.html","./utrpeni_mladeho_werthera_006.html","./utrpeni_mladeho_werthera_007.html","./utrpeni_mladeho_werthera_008.html","./utrpeni_mladeho_werthera_009.html","./utrpeni_mladeho_werthera_010.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.png","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
+        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./promo.html","./utrpeni_mladeho_werthera_005.html","./utrpeni_mladeho_werthera_006.html","./utrpeni_mladeho_werthera_007.html","./utrpeni_mladeho_werthera_008.html","./utrpeni_mladeho_werthera_009.html","./utrpeni_mladeho_werthera_010.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/LiterataTT-TextSemibold.woff2","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.png","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
     })
         .then(() => idb.set({ key: 'cacheUpdatedAt', value: new Date().toISOString() }))
         .then(() => {
